24e4d53d2bf069795bd01ebbda89e344c576fc05
Gayatri Hazarika <gayatri.hazarika@vantagecircle.com>
2020-02-28 15:37:58 +0530

    changes done

--- assets/js/customjs.js
+++ assets/js/customjs.js
@@ -45,6 +45,7 @@
             
     
         });
+        $('[data-toggle="popover"]').popover(); 
 
             
 
@@ -56,6 +57,7 @@ jQuery(function ($) {
             $(this).addClass('active');
         }
     });
+
     
 
 });
